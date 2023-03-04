d5c902e48c1feffb8a4c2a6141e2cab3512a3bdb
Roger <rsd.santo@outlook.com>
2023-03-04 05:43:13 -0300

    added new script

--- src/instances/mysql.ts
+++ src/instances/mysql.ts
@@ -1,24 +1,24 @@
-import { Sequelize } from "sequelize";
-import dotenv from 'dotenv'
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 export const sequelize = new Sequelize(
-    process.env.DATABASE_NAME as string,
-    process.env.DATABASE_USER as string,
-    process.env.DATABASE_PASSWORD as string,
+  process.env.DATABASE_NAME as string,
+  process.env.DATABASE_USER as string,
+  process.env.DATABASE_PASSWORD as string,
 
-    {
-        dialect:'mysql',
-        port:parseInt(process.env.DATABASE_PORT as string),
-    }
-)
+  {
+    dialect: 'mysql',
+    port: parseInt(process.env.DATABASE_PORT as string),
+  }
+);
 
-export const connection = async ()=>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-connection()
+export const connection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+connection();
